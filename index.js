ead3699be8683bc3a7f8876ec161d94ae5f336eb
ParikhGuleria <parikhguleria801@gmail.com>
2025-04-12 18:09:42 +0530

    index change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use('/api',require('./Routes/CreateUser'));
 app.use('/api',require('./Routes/DisplayData'));
-app.use('/api',require('./Routes/orderData'));
+app.use('/api',require('./Routes/OrderData'));
 
 
 app.listen(PORT, () => {

33840e7f276ad8bb47126c5cabc44731920eb6fe
ParikhGuleria <parikhguleria801@gmail.com>
2025-04-13 17:53:48 +0530

    last commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = 5000
 const connectToMongo=require("./db")
 var cors=require('cors');
 

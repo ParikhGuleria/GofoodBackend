e852b0777f33b5e53b8bc222d50506a4314a6953
ParikhGuleria <parikhguleria801@gmail.com>
2025-04-13 22:05:05 +0530

    b

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 5000
 const connectToMongo=require("./db")
 var cors=require('cors');
 
-app.use(cors);
+app.use(cors());
 
 connectToMongo();
 app.use(express.json());
@@ -19,7 +19,8 @@ app.use('/api',require('./Routes/DisplayData'));
 app.use('/api',require('./Routes/OrderData'));
 
 
-app.listen(port,() => {
-  console.log(`Example: app listening on port: ${port}`)
-})
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Example: app listening on port: ${port}`);
+});
+
 
